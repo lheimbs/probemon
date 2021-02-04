c4cd60e230f6af43c618e5d3796ea16311f282fd
Lennart Heimbs <l.heimbs@gmail.com>
2021-02-04 11:28:08 +0100

    Add sql and mqtt classes

--- probemon/mqtt/__init__.py
+++ probemon/mqtt/__init__.py
@@ -7,29 +7,58 @@ logger = logging.getLogger('mqtt')
 class Mqtt(object):
     """ Mqtt class to handle client connection and publishing """
 
-    def __init__(self, enabled):
+    def __init__(self, enabled=False):
+        """initiate the mqtt client, """
         self._client = mqtt_client.Client()
         self.__enabled = enabled
 
-    def set_server(self, host, port):
+    def is_enabled(self):
+        return self.__enabled
+
+    def enable(self):
+        self.__enabled = True
+
+    def set_server(self, host, port=1883):
+        if not host:
+            logger.error(
+                "Can't configure Mqtt-Client because no host is supplied. "
+                "Disabling mqtt!"
+            )
+            self.__enabled = False
+            return
+
+        if not port:
+            logger.info("No port supplied. Using default port 1883.")
+
+        logger.debug(f"Setting mqtt host {host} with port {port}.")
         self.__host = host
         self.__port = port
 
     def set_user(self, user, password):
+        logger.debug(f"Setting mqtt user {user} with password {password}.")
         self._client.username_pw_set(user, password)
 
     def set_tls(self, ca_certs, certfile, keyfile):
-        self._client.tls_set(ca_certs=ca_certs, certfile=certfile, keyfile=keyfile)
+        if ca_certs or (certfile and keyfile):
+            logger.debug(
+                "Setting tls encryption with "
+                f"ca_certs file {ca_certs}, "
+                f"certfile {certfile} and keyfile {keyfile}."
+            )
+            self._client.tls_set(ca_certs=ca_certs, certfile=certfile, keyfile=keyfile)
 
     def enable_logger(self, log_level):
-        logger.setLevel(log_level)
-        self._client.enable_logger(logger)
+        logger.debug(f"Enabling mqtt logger with loglevel {log_level}.")
+        self._client.enable_logger(logger.setLevel(log_level))
 
     def connect(self):
+        logger.debug("Connecting to mqtt broker.")
         self._client.connect(self.__host, port=self.__port)
 
     def start(self):
+        logger.debug("Starting mqtt network loop.")
         self._client.loop_start()
 
     def publish_probe(self, probe_request):
+        logger.debug("Publishing ProbeRequest to mqtt.")
         self._client.publish(json.dumps(probe_request))
--- probemon/sql/__init__.py
+++ probemon/sql/__init__.py
@@ -1,13 +1,55 @@
 import logging
 import sqlalchemy
+from sqlalchemy.orm.session import Session
+from contextlib import contextmanager
 
-logger = 
+logger = logging.getLogger('sql')
 
 class Sql:
     """ Class representing the sql connection """
 
-    def __init__(self):
-        self.__url = ""
+    def __init__(self, enabled=False):
+        self._engine = None
+        self.__enabled = enabled
 
-    def set_url(self, 
+    # def enable(self):
+    #     self.__enabled = True
 
+    def is_enabled(self):
+        return self.__enabled
+
+    def set_url(self, url):
+        try:
+            self._engine = sqlalchemy.create_engine(url)
+            self.__enabled = True
+        except ModuleNotFoundError:
+            logger.exception(
+                "Could not create sql connection because a neccessary python module is missing."
+            )
+            self._engine = None
+
+    def register_engine(self, Session):
+        if self._engine is not None:
+            Session.configure(bind=self._engine)
+            self.__enabled = True
+        else:
+            logger.error(
+                "Can't configure Sql-Session because the engine is not configured. "
+                "Disabling sql!"
+            )
+            self.__enabled = False
+
+
+@contextmanager
+def session_scope():
+    """Provide a transactional scope around a series of operations."""
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:         # noqa: E722
+        logger.exception("Exception occured during sql operation!")
+        session.rollback()
+        raise
+    finally:
+        session.close()
